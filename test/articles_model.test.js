bad9ebc78b40737323965808672c5d1aba25c28d
David Yang <david.g.yang@gmail.com>
2014-10-06 21:43:54 -0400

    add some refs to documentation

--- test/articles_model.test.js
+++ test/articles_model.test.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose')
 /**
  * Start here
  *
- * These tests describe the model that you'll be setting up in app/models/article.js
+ * These tests describe the model that you'll be setting up in models/article.js
  *
  */
 
@@ -12,6 +12,8 @@ describe('Articles', function () {
 
   /**
    * Your model should have two fields (both required) of title and body
+   *
+   * Check out the 'required' validator: http://mongoosejs.com/docs/api.html#schematype_SchemaType-required
    */
   it('should have a title and body field of String', function (done) {
     var article = new Article({title: "My Article", body: "Isn't this interesting?"});
@@ -33,6 +35,8 @@ describe('Articles', function () {
   /**
    * Set up an instance method (check out mongoose .methods) called asJSON that
    * will output the JSON representation of the model
+   *
+   * http://mongoosejs.com/docs/guide.html#methods
    */
   it('should have an instance method to get itself as JSON', function(done) {
     Article.findOne({title: "My Article"}, function(err, article) {
@@ -46,6 +50,8 @@ describe('Articles', function () {
   /**
    * Set up a static method called findByTitle that's a convenience
    * method to find by a title
+   *
+   * http://mongoosejs.com/docs/guide.html#statics
    */
   it('should have a static method to findByTitle', function(done) {
     Article.findByTitle("My Article", function(err, article) {
@@ -59,7 +65,7 @@ describe('Articles', function () {
    * Your Article model should also have a tag field that's an array
    * but when we access it, we should get the string joined by a comma
    *
-   * Look at using getters in your Schema
+   * Look at using getters in your Schema (under Virtuals at http://mongoosejs.com/docs/guide.html)
    */
   it('should have a tags field of [] that has a custom getter', function (done) {
     var article = new Article({ title: "Taggy", body: "So Taggy" });
