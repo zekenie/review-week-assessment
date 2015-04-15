4e98790e42b1a4bf1a14ff3853a7fe435b62228c
Zeke Nierenberg <hampzan09@gmail.com>
2015-04-15 10:33:55 -0400

    gutting express section

--- models/article.js
+++ models/article.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/assessjs');
+mongoose.connect('mongodb://localhost/assessjs2');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
